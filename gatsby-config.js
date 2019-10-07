9fa1c2798de9306d8ac5f00d6b6af27a0224b67d
davewelsh <dave@qccareerschool.com>
2019-10-07 11:51:29 -0400

    Removed gatsby images and added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#e70260`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/qcma-favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
