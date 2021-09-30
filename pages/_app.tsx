2de889c0472ec05c1b3b224ccf8784760a976f59
Dave Welsh <dave@qccareerschool.com>
2021-09-29 20:09:13 -0400

    Add Google Analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
 import '../styles/global.scss';
 
 import type { AppProps } from 'next/app';
-import { ReactElement } from 'react';
+import { useRouter } from 'next/router';
+import { ReactElement, useEffect } from 'react';
 
+import * as ga from '../lib/ga';
 import { LocationProvider } from '../providers/LocationProvider';
 import { ScreenWidthProvider } from '../providers/ScreenWidthProvider';
 import { ScrollPositionProvider } from '../providers/ScrollPositionProvider';
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string): void => {
+      ga.pageview(url);
+    };
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [ router.events ]);
+
   return (
     <LocationProvider>
       <ScreenWidthProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,18 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="h-100" prefix="og: http://ogp.me/ns#">
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{ __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { page_path: window.location.pathname });
+            ` }}
+          />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Playfair+Display:400,700,900&display=swap" />
         </Head>
         <body className="d-flex flex-column h-100">
